4dcb9580ab91e4db12038e1189a4230a496ce0e9
Poojaravi25 <155242530+Poojaravi25@users.noreply.github.com>
2026-06-26 23:49:53 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,27 @@
+-- Database: datawarehouse
+
+DROP DATABASE IF EXISTS datawarehouse;
+
+CREATE DATABASE datawarehouse
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+GO
+
+USE datawarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO 
+
+CREATE SCHEMA silver;
+GO 
+
+CREATE SCHEMA gold;
+GO 
